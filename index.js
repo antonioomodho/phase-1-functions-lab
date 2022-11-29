5f7e1f66ac79ef7cd9e2eea01c0c84097c78e41b
antonioomodho <antonioomodho@gmail.com>
2022-11-30 00:22:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location)
+{
+    if (location == 43){
+        return (1);
+    }else {
+        return (8);
+    }
+}
+function distanceFromHqInFeet(block)
+{
+    if (block == 43)
+    { const feetDistance  = (block - 42) * 264;
+        return feetDistance;
+    }else {
+        return (2112);
+    }
+}
+function distanceTravelledInFeet(x,y)
+{
+    if (y > x){
+    const distance = (y - x) * 264;
+    return (distance);
+    }
+    else if (x > y){
+        const distance = (x - y) * 264;
+        return  (distance);
+    }
+}
+function calculatesFarePrice(start, destination)
+{
+    const dist =  distanceTravelledInFeet( start,destination)
+    let n;
+    if (dist <=400) {
+        n = 0;
+    }
+     else if (dist > 400 && dist < 2000){   
+        n = 2.56;   
+    }else if (dist > 2000 && dist < 2500 ){
+        n = 25;
+     }
+     else  if ( dist > 2500) {
+         n = 'cannot travel that far';
+    }
+    return n;
+}
